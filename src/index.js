c6189e01a22f98e84db0535ca1b9c10f0062c4c8
Павел Мочалов <78229095+010MochalovPA@users.noreply.github.com>
2021-03-05 22:25:15 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let countMistakes;
+    do {
+        countMistakes = 0;
+        for(let item in bracketsConfig){
+            if (str.indexOf(bracketsConfig[item].join(''))!= -1){
+                str = str.replace(bracketsConfig[item].join(''),'');
+            }
+            else { countMistakes++ }           
+        }
+    } while (str!='' && countMistakes < bracketsConfig.length);
+    return (str!='') ? false : true ;
 }
